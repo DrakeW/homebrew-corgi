7a0da2e12af0337e72a7eac76e809e738e81a46f
Junyu Wang <junyuw@berkeley.edu>
2018-09-15 23:44:52 -0700

    v0.2.4 release

--- Formula/corgi.rb
+++ Formula/corgi.rb
@@ -3,9 +3,9 @@
 class Corgi < Formula
   desc "Corgi is a command-line workflow manager that helps with your repetitive command usages by organizing them into reusable snippet"
   homepage "https://github.com/DrakeW/corgi"
-  url "https://github.com/DrakeW/corgi/releases/download/v0.2.3/corgi_v0.2.3_macOS_64-bit.tar.gz"
-  version "0.2.3"
-  sha256 "b5ae0ce16d979a5f466725eb1ea67776f62a37c17b64a4f2ddd00d36c2e4bafa"
+  url "https://github.com/DrakeW/corgi/releases/download/v0.2.4/corgi_v0.2.4_macOS_64-bit.tar.gz"
+  version "0.2.4"
+  sha256 "6bab64f774db25abf3030f583df3f18c302c8e10f6bca2905e9e2513329d7fb7"
 
   depends_on "fzf"
 
